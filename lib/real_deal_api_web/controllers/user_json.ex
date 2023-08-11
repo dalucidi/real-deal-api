5572fb2eea554e94dd8d6b45ee7b1eb4da7a3f78
Daniel <dalucidi@gmail.com>
2023-08-11 00:18:57 -0400

    Make authorization a plug instead of a controller function. Add user update.

--- lib/real_deal_api_web/controllers/user_json.ex
+++ lib/real_deal_api_web/controllers/user_json.ex
@@ -15,12 +15,19 @@ defmodule RealDealApiWeb.UserJSON do
     %{data: data(user)}
   end
 
-  def data(%{user: user}) do
+  defp data(%User{} = user) do
     %{
       id: user.id,
       full_name: user.full_name,
       gender: user.gender,
       biography: user.biography
+    }
+  end
+
+  def user(%{user: user, token: token}) do
+    %{
+      id: user.id,
+      token: token
     }
   end
 end
