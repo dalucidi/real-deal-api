5c1838cc70e36ebc81f826c3f2fa9044275fd070
Daniel <dalucidi@gmail.com>
2023-08-13 15:14:24 -0400

    Add unit tests to check validity of field types for account and user

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :real_deal_api, RealDealApi.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "dalucidi",
+  password: "aN10N6n0w31p4nWa",
   hostname: "localhost",
   database: "real_deal_api_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
--- lib/real_deal_api/accounts/account.ex
+++ lib/real_deal_api/accounts/account.ex
@@ -2,6 +2,7 @@ defmodule RealDealApi.Accounts.Account do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @optional_fields [:id, :inserted_at, :updated_at]
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "accounts" do
@@ -12,11 +13,15 @@ defmodule RealDealApi.Accounts.Account do
     timestamps()
   end
 
+  defp all_fields do
+    __MODULE__.__schema__(:fields)
+  end
+
   @doc false
   def changeset(account, attrs) do
     account
-    |> cast(attrs, [:email, :hashed_password])
-    |> validate_required([:email, :hashed_password])
+    |> cast(attrs, all_fields())
+    |> validate_required(all_fields() -- @optional_fields)
     |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
     |> validate_length(:email, max: 160)
     |> unique_constraint(:email)
